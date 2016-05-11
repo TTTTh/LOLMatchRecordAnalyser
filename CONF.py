86d49646afc8f4104c521d42a739144e2374a592
TTTTh <thbjay@gmail.com>
2016-05-12 01:19:55 +0800

    开始
    23423

---
+++ CONF.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+# CONF.py
+
+TOP_PATH = '/volumes/ExtraData/LOLMatchRecordAnalyser/'
+HTML_PATH = TOP_PATH + 'RecordHtmls/'
+JSON_PATH = TOP_PATH + 'RecordJsons/'
+
+PLAYER_INFO = TOP_PATH +'PlayerInfo.json'
+COMPLETED_LIST = TOP_PATH + 'CompletedList.json'
---
+++ Spider.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+# Spider.py
+# 爬虫，实际工作者
+
+import CONF
+import json
+
+if __name__ == '__main__':
+    print 'crawler runner beggin'
